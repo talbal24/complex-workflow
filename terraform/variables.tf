4c0b87d3de4e10b3326305ee8803ab939e80d663
talbal24 <134201605+talbal24@users.noreply.github.com>
2026-04-28 16:55:52 +0300

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "environment_name" {
+  description = "Tag name for the environment"
+  type        = string
+  default     = "acme-financial-demo"
+}
