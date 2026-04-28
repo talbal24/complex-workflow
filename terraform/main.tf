6c02e9c47ffb4ca3532727c17fbfe7dbd7a50f6d
talbal24 <134201605+talbal24@users.noreply.github.com>
2026-04-28 17:12:33 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,18 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_instance" "acme_app" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
-  tags = { Name = var.environment_name }
+  tags          = { Name = var.environment_name }
 }
